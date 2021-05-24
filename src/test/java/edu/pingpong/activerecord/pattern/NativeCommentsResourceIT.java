8f730c523c8929dc2b5e0cbc553a5db6c95b3e7c
pau <pllinas@cifpfbmoll.eu>
2021-05-24 21:16:24 +0200

    build: :tada: Create quarkus project

---
+++ src/test/java/edu/pingpong/activerecord/pattern/NativeCommentsResourceIT.java
@@ -0,0 +1,9 @@
+package edu.pingpong.activerecord.pattern;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCommentsResourceIT extends CommentsResourceTest {
+
+    // Execute the same tests but in native mode.
+}
